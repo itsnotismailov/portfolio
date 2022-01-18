1312f10c0528a43170221b5334e41baa0117ab40
itsnotismailov <ismailov@gmail.com>
2022-01-18 15:56:25 +0600

    начинаю навбар

--- portfolio/src/Routes.js
+++ portfolio/src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import MainPage from './pages/MainPage'
 
 const MyRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/home" element={<About/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/home" element={<MainPage/>}/>
             </Routes>
         </BrowserRouter>
     );
--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 const About = () => {
-  return <div>о</div>;
+  return (
+    <div>
+      <a href="/">
+        <Button>About me</Button>
+        <Button>Contacts</Button>
+      </a>
+    </div>
+  );
 };
 
 export default About;
